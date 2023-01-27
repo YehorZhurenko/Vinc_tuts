fb0d30ae5f6ef18ea8f01d1ed7f948a6379d68b2
Yehor Zhurenko <yehorzhurenko@gmail.com>
2023-01-27 16:15:25 +0200

    new

--- code/blogapi/config/settings.py
+++ code/blogapi/config/settings.py
@@ -33,14 +33,14 @@ INSTALLED_APPS = [
     'posts',
 ]
 
-REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ]}
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', ]}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]

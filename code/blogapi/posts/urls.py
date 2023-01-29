ca7c2e3f6f4c68d4a1fa59756c33b97c107f8797
Yehor Zhurenko <yehorzhurenko@gmail.com>
2023-01-29 19:25:11 +0200

    fin2

--- code/blogapi/posts/urls.py
+++ code/blogapi/posts/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from .views import UserViewSet, PostViewSet
+from .views import * 
 
-router = SimpleRouter()
+""" router = SimpleRouter()
 
 router.register('users', UserViewSet, basename='users')
 router.register('', PostViewSet, basename='posts')
 
-urlpatterns = router.urls
+urlpatterns = router.urls """
+
+urlpatterns = [
+
+    path('users/', UserList.as_view()), # new
+    path('users/<int:pk>/', UserDetail.as_view()), # new
+    path('', PostList.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
+    path('venue_pdf/<int:pk>', venue_pdf, name = 'venue_pdf'),
+
+]
--- code/blogapi/posts/views.py
+++ code/blogapi/posts/views.py
@@ -1,14 +1,68 @@
-from django.contrib.auth import get_user_model
-from rest_framework import viewsets # new
+from django.http import FileResponse, HttpResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+from django.contrib.auth import get_user_model # new
+from rest_framework import generics
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer, UserSerializer
+from .serializers import PostSerializer, UserSerializer # new
+from rest_framework.decorators import permission_classes, api_view, authentication_classes
+from rest_framework.permissions import IsAuthenticated
 
-class PostViewSet(viewsets.ModelViewSet): # new
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated, ))
+def venue_pdf(request, pk):
+
+    if Post.objects.get(pk=pk).author == request.user:
+    
+        posts = Post.objects.get(pk=pk)
+
+        buf = io.BytesIO()
+        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        textob = c.beginText()
+        textob.setTextOrigin(inch, inch)
+        textob.setFont("Helvetica", 14)
+
+        lines = []
+            
+        lines.append('Title: ' + posts.title)
+        lines.append('Body: ' + posts.body)
+        lines.append("  ")
+        
+        for line in lines:
+            textob.textLine(line)
+
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        return FileResponse(buf, as_attachment=True, filename='venue.pdf')
+    
+    else:
+
+        return HttpResponse(
+            "not your post")
+        
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class UserViewSet(viewsets.ModelViewSet): # new
+class UserList(generics.ListCreateAPIView): # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView): # new
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
